ca6d9ea2b80505ae4256eca9d0e9191e342d0c0b
Raizento <it.alexander.molitor@gmail.com>
2025-01-01 21:21:49 +0100

    Day 4; diagonal checking

--- aoc4/src/lib.rs
+++ aoc4/src/lib.rs
@@ -50,7 +50,46 @@ pub fn count_occurences_of_xmas_horizontal(text: &str) -> u32 {
 }
 
 pub fn count_occurences_of_xmas_diagonal(text: &str) -> u32 {
-    0
+    let mut occurences = 0;
+
+    let lines = text.lines().map(|line| line.as_bytes()).collect::<Vec<&[u8]>>();
+
+    for window in lines.windows(4) {
+        for i in 0..window[0].len()-3 {
+            // left to right
+            if window[0][i] == b'X'
+            && window[1][i+1] == b'M'
+            && window[2][i+2] == b'A'
+            && window[3][i+3] == b'S' {
+                occurences += 1;
+            }
+
+            if window[0][i] == b'S'
+            && window[1][i+1] == b'A'
+            && window[2][i+2] == b'M'
+            && window[3][i+3] == b'X' {
+                occurences += 1;
+            }
+
+            // right to left
+
+            if window[0][i+3] == b'X'
+            && window[1][i+2] == b'M'
+            && window[2][i+1] == b'A'
+            && window[3][i] == b'S' {
+                occurences += 1;
+            }
+
+            if window[0][i+3] == b'S'
+            && window[1][i+2] == b'A'
+            && window[2][i+1] == b'M'
+            && window[3][i] == b'X' {
+                occurences += 1;
+            }
+        }
+    }
+
+    occurences
 }
 
 #[test]
@@ -129,4 +168,46 @@ fn test_count_occurences_of_xmas_vertical_multiple_xmas() {
     let occurences = count_occurences_of_xmas_vertical(text);
 
     assert_eq!(3, occurences);
+}
+
+#[test]
+fn test_count_occurences_of_xmas_diagonal_single_xmas_forward() {
+    let text = "X...\n\
+                      .M..\n\
+                      ..A.\n\
+                      ...S";
+
+    let occurences = count_occurences_of_xmas_diagonal(text);
+
+    assert_eq!(1, occurences);
+}
+
+#[test]
+fn test_count_occurences_of_xmas_diagonal_single_xmas_backward() {
+    let text = "S...\n\
+                      .A..\n\
+                      ..M.\n\
+                      ...X";
+
+    let occurences = count_occurences_of_xmas_diagonal(text);
+
+    assert_eq!(1, occurences);
+}
+
+#[test]
+fn test_count_occurences_of_xmas_diagonal_multiple_xmas() {
+    let text = "MMMSXXMASM\n\
+                       MSAMXMSMSA\n\
+                       AMXSXMAAMM\n\
+                       MSAMASMSMX\n\
+                       XMASAMXAMM\n\
+                       XXAMMXXAMA\n\
+                       SMSMSASXSS\n\
+                       SAXAMASAAA\n\
+                       MAMMMXMMMM\n\
+                       MXMXAXMASX";
+
+    let occurences = count_occurences_of_xmas_diagonal(text);
+
+    assert_eq!(10, occurences);
 }
